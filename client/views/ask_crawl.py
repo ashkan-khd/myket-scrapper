49fe03fe71cfbb12286a48a9cd448871cdbb0cd8
Ashkan Khademian <ashkan.khd.q@gmail.com>
2024-05-20 14:55:53 +0330

    feat: init client app

---
+++ client/views/ask_crawl.py
@@ -0,0 +1,17 @@
+from rest_framework.mixins import CreateModelMixin
+from rest_framework.settings import api_settings
+from rest_framework.viewsets import GenericViewSet
+
+from crawl.models import MovieCode, Movie
+from crawl.serializers import MovieCodeDeserializer
+
+
+class AskCrawlAPI(CreateModelMixin, GenericViewSet):
+    queryset = MovieCode.objects.all()
+    permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES + []
+    serializer_class = MovieCodeDeserializer
+
+    def perform_create(self, serializer):
+        movie_code: MovieCode = serializer.save()
+        crawler = movie_code.get_crawler()
+        crawler.start_crawling()
---
+++ client/views/retrieve_movie.py
@@ -0,0 +1,22 @@
+from rest_framework.mixins import RetrieveModelMixin
+from rest_framework.settings import api_settings
+from rest_framework.viewsets import GenericViewSet
+
+from crawl.models import MovieCode, Movie
+from crawl.serializers import MovieSerializer, MovieCodeSerializer
+
+
+class GetMovieCodeView(RetrieveModelMixin, GenericViewSet):
+    queryset = MovieCode.objects.all()
+    permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES + []
+    lookup_field = "code"
+    lookup_url_kwarg = "code"
+    serializer_class = MovieCodeSerializer
+
+
+class GetMovieView(RetrieveModelMixin, GenericViewSet):
+    queryset = Movie.objects.all()
+    permission_classes = api_settings.DEFAULT_PERMISSION_CLASSES + []
+    lookup_field = "code"
+    lookup_url_kwarg = "code"
+    serializer_class = MovieSerializer
